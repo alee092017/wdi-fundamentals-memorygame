747053d7bd2b820ea9211f15ac221531011eacf9
6HALUSER <andrealee@User2616HAL.local>
2017-09-24 17:44:12 -0400

    DONE-so.many.typos

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,34 +1,75 @@
 var cards = [
 {
-rank: "queen",
-suit: "hearts",
-cardImage: "images/queen-of-hearts.png"
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
 },
 {
-rank: "queen",
-suit: "diamonds",
-cardImage: "images/queen-of-diamonds.png"
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
 },
 {
-rank: "king",
-suit: "hearts",
-cardImage: "images/king-of-hearts.png"
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
 },
 {
-rank: "king",
-suit: "diamonds",
-cardImage: "images/king-of-diamonds.png"
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
 }
 ];
+
 var cardsInPlay = [];
 
 var checkForMatch = function() {
-if (cardsInPlay[0] === cardsInPlay[1]) {
+
+	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 		} else {
-			alert("Sorry, try again.");
+		alert("Sorry, try again.");
 	}	
-}
+};
+
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
+	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}	
+	
+};
+	
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click',flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+};
+
+createBoard();
+
+
+
+/*
+When assignment instructions say to delete 
+the code you wrote in the last assignment:
+
+From Assignment 11:
+
+TOOK HOURS TO FIND THAT I'D CAPITALIZED THE D IN ID
+
+flipCard before changes:
+
 var flipCard = function(cardID) {
 console.log("User flipped " + cards[cardID].rank);
 cardsInPlay.push(cards[cardID].rank);
@@ -37,14 +78,11 @@ if (cardsInPlay.length === 2) {
 	checkForMatch();
 	}
 };
-flipCard(0);
-flipCard(2);
 
+variation on createBoard:
+var board = document.getElementById('game-board');
+		board.appendChild(cardElement);
 
-
-/*
-When assignment instructions say to delete 
-the code you wrote in the last assignment:
 From Assignment 10:
 var cards = ["queen", "queen", "king", "king"];
 
